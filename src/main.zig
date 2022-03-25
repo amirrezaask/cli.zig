0650219bbe9b26bf13d755f883d7c462db6e6e56
AmirrezaAsk <raskarpour@gmail.com>
2022-03-25 16:33:48 +0430

    tests

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,30 @@
 const std = @import("std");
+const print = std.debug.print;
 
 pub const Args = [][]const u8;
-pub const Flags = std.AutoHashMap([]u8, []u8);
+pub const Flags = std.StringHashMap([]const u8);
 
 pub const Parsed = struct {
+    allocator: std.mem.Allocator,
     args: Args,
     flags: Flags,
+    pub fn deinit(self: *Parsed) void {
+        self.flags.deinit();
+        self.allocator.free(self.args);
+    }
 };
 const FlagType = enum {
     single_dash,
     double_dash,
 };
-fn is_flag(arg: []u8) ?FlagType {
+fn is_flag(arg: []const u8) ?FlagType {
     if (arg[0] == '-' and arg[1] == '-') return FlagType.double_dash;
     if (arg[0] == '-' and arg[1] != '-') return FlagType.single_dash;
     return null;
 }
 
-fn parse(allocator: std.mem.Allocator, aruguments: [][]u8) !Parsed {
-    var args = std.ArrayList([]u8).init(allocator);
+fn parse(allocator: std.mem.Allocator, aruguments: [][]const u8) !Parsed {
+    var args = std.ArrayList([]const u8).init(allocator);
     var flags = Flags.init(allocator);
     var i: usize = 0;
     while (i < aruguments.len) : (i += 1) {
@@ -41,13 +47,69 @@ fn parse(allocator: std.mem.Allocator, aruguments: [][]u8) !Parsed {
         }
     }
     return Parsed{
+        .allocator = allocator,
         .args = args.items,
         .flags = flags,
     };
 }
 
-test "sample" {
+test "cli.argument should be added to args array when parsed" {
     const a = std.testing.allocator;
-    const parsed = try parse(a, &[_][]u8{ "a", "--b", "b", "-c", "c" });
-    std.testing.expectEqualStrings("b", parsed.flags.get("b").?);
+    var args = std.ArrayList([]const u8).init(a);
+    defer args.deinit();
+    try args.append("a");
+    var parsed = try parse(a, args.items);
+    defer parsed.deinit();
+    try std.testing.expectEqualStrings("a", parsed.args[0]);
+}
+test "cli.single dash flag should be added to flags map" {
+    const a = std.testing.allocator;
+    var args = std.ArrayList([]const u8).init(a);
+    defer args.deinit();
+    try args.append("-b");
+    try args.append("b");
+    var parsed = try parse(a, args.items);
+    defer parsed.deinit();
+    try std.testing.expectEqualStrings("b", parsed.flags.get("b").?);
+}
+
+test "cli.double dash flag should be added to flags map" {
+    const a = std.testing.allocator;
+    var args = std.ArrayList([]const u8).init(a);
+    defer args.deinit();
+    try args.append("--b");
+    try args.append("b");
+    var parsed = try parse(a, args.items);
+    defer parsed.deinit();
+    try std.testing.expectEqualStrings("b", parsed.flags.get("b").?);
+}
+
+test "cli.mixed flags" {
+    const a = std.testing.allocator;
+    var args = std.ArrayList([]const u8).init(a);
+    defer args.deinit();
+    try args.append("--b");
+    try args.append("b");
+    try args.append("-c");
+    try args.append("b");
+    var parsed = try parse(a, args.items);
+    defer parsed.deinit();
+    try std.testing.expectEqualStrings("b", parsed.flags.get("b").?);
+    try std.testing.expectEqualStrings("b", parsed.flags.get("c").?);
+}
+
+test "cli.mixed flags and args" {
+    const a = std.testing.allocator;
+    var args = std.ArrayList([]const u8).init(a);
+    defer args.deinit();
+    try args.append("a");
+    try args.append("--b");
+    try args.append("b");
+    try args.append("-c");
+    try args.append("b");
+    var parsed = try parse(a, args.items);
+    defer parsed.deinit();
+    try std.testing.expectEqualStrings("b", parsed.flags.get("b").?);
+    try std.testing.expectEqualStrings("b", parsed.flags.get("c").?);
+    try std.testing.expectEqualStrings("a", parsed.args[0]);
 }
