e57f082e676efa71c38e9242d00358c879f01e5c
AmirrezaAsk <raskarpour@gmail.com>
2022-03-25 05:57:42 +0430

    Initial commit

--- src/main.zig
+++ src/main.zig
@@ -7,8 +7,47 @@ pub const Parsed = struct {
     args: Args,
     flags: Flags,
 };
+const FlagType = enum {
+    single_dash,
+    double_dash,
+};
+fn is_flag(arg: []u8) ?FlagType {
+    if (arg[0] == '-' and arg[1] == '-') return FlagType.double_dash;
+    if (arg[0] == '-' and arg[1] != '-') return FlagType.single_dash;
+    return null;
+}
 
+fn parse(allocator: std.mem.Allocator, aruguments: [][]u8) !Parsed {
+    var args = std.ArrayList([]u8).init(allocator);
+    var flags = Flags.init(allocator);
+    var i: usize = 0;
+    while (i < aruguments.len) : (i += 1) {
+        const arg = aruguments[i];
+        if (is_flag(arg)) |is_flag_val| {
+            switch (is_flag_val) {
+                .single_dash => {
+                    try flags.put(arg[1..], aruguments[i + 1]);
+                    i += 1;
+                    continue;
+                },
+                .double_dash => {
+                    try flags.put(arg[2..], aruguments[i + 1]);
+                    i += 1;
+                    continue;
+                },
+            }
+        } else {
+            try args.append(arg);
+        }
+    }
+    return Parsed{
+        .args = args.items,
+        .flags = flags,
+    };
+}
 
-fn parse(aruguments: [][]u8) !Parsed {
-    
+test "sample" {
+    const a = std.testing.allocator;
+    const parsed = try parse(a, &[_][]u8{ "a", "--b", "b", "-c", "c" });
+    std.testing.expectEqualStrings("b", parsed.flags.get("b").?);
 }
